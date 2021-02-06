6f0ba0b7eb8b9a7c6cbf14118a9c27d3a195f686
Ian Macaulay <ismacaul@gmail.com>
2021-02-05 17:34:16 -0700

    Updated noise generator test

--- src/cpp/tests/test_noise_generator.cpp
+++ src/cpp/tests/test_noise_generator.cpp
@@ -1,13 +1,16 @@
 #include <catch2/catch.hpp>
 
+#include "noise/noise_generator.h"
 #include "toolkit/noise.h"
 
 TEST_CASE("can generate perlin noise", "[noise generator]")
 {
     size_t width = 100;
     size_t height = 100;
-    float* noise =
-        tk::noise::generate_perlin_noise(width, height, 0.3);
+
+    tk::noise::NoiseGenerator generator(width, height);
+    generator.set_scale(0.3);
+    float* noise = generator.generate();
 
     for (size_t y = 0; y < height; ++y) {
         for (size_t x = 0; x < width; ++x) {
