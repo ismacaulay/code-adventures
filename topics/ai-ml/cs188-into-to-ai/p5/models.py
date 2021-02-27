08b0371bdd563570d29cca19af132828ad00949a
Ian Macaulay <ismacaul@gmail.com>
2021-02-27 14:39:04 -0700

    [cs188-p5] Completed q3/4
    I dont fully understand the parameter tuning, so I used some github
    help, but hopefully that will come with more experience with neural nets

--- topics/ai-ml/cs188-into-to-ai/p5/models.py
+++ topics/ai-ml/cs188-into-to-ai/p5/models.py
@@ -51,6 +51,22 @@ class PerceptronModel(object):
             if not mistake:
                 break
 
+def runWithParams(x, params):
+    layer = None
+    layerCount = int(len(params) / 2)
+    for i in range(layerCount):
+        w = params[i * 2 + 0]
+        b = params[i * 2 + 1]
+        if layer == None:
+            layer = nn.AddBias(nn.Linear(x, w), b)
+        else:
+            layer = nn.AddBias(nn.Linear(layer, w), b)
+
+        if i < layerCount - 1:
+            layer = nn.ReLU(layer)
+
+    return layer
+
 class RegressionModel(object):
     """
     A neural network model for approximating a function that maps from real
@@ -67,7 +83,8 @@ class RegressionModel(object):
         self.trainEndLoss = 0.015
         self.params = [self.w1, self.b1, self.w2, self.b2]
 
-
+        # taken from https://github.com/zhiming-xu/CS188/blob/master/p5-ml/models.py as I still
+        # dont fully understand how to choose these values
         # self.w1 = nn.Parameter(1, 128)
         # self.b1 = nn.Parameter(1, 128)
         # self.w2 = nn.Parameter(128, 64)
@@ -86,20 +103,7 @@ class RegressionModel(object):
         Returns:
             A node with shape (batch_size x 1) containing predicted y-values
         """
-        layer = None
-        layerCount = int(len(self.params) / 2)
-        for i in range(layerCount):
-            w = self.params[i * 2 + 0]
-            b = self.params[i * 2 + 1]
-            if layer == None:
-                layer = nn.AddBias(nn.Linear(x, w), b)
-            else:
-                layer = nn.AddBias(nn.Linear(layer, w), b)
-
-            if i < layerCount - 1:
-                layer = nn.ReLU(layer)
-
-        return layer
+        return runWithParams(x, self.params)
 
     def get_loss(self, x, y):
         """
@@ -144,7 +148,20 @@ class DigitClassificationModel(object):
     """
     def __init__(self):
         # Initialize your model parameters here
-        "*** YOUR CODE HERE ***"
+
+        # taken from https://github.com/zhiming-xu/CS188/blob/master/p5-ml/models.py as I still
+        # dont fully understand how to choose these values
+        self.w1 = nn.Parameter(784, 256)
+        self.b1 = nn.Parameter(1, 256)
+        self.w2 = nn.Parameter(256, 128)
+        self.b2 = nn.Parameter(1, 128)
+        self.w3 = nn.Parameter(128, 64)
+        self.b3 = nn.Parameter(1, 64)
+        self.w4 = nn.Parameter(64, 10)
+        self.b4 = nn.Parameter(1, 10)
+        self.params = [self.w1, self.b1, self.w2, self.b2, self.w3, self.b3, self.w4, self.b4]
+        self.learningRate = 0.1
+        self.batchSize = 100
 
     def run(self, x):
         """
@@ -160,7 +177,7 @@ class DigitClassificationModel(object):
             A node with shape (batch_size x 10) containing predicted scores
                 (also called logits)
         """
-        "*** YOUR CODE HERE ***"
+        return runWithParams(x, self.params)
 
     def get_loss(self, x, y):
         """
@@ -175,13 +192,20 @@ class DigitClassificationModel(object):
             y: a node with shape (batch_size x 10)
         Returns: a loss node
         """
-        "*** YOUR CODE HERE ***"
+        return nn.SoftmaxLoss(self.run(x), y)
 
     def train(self, dataset):
         """
         Trains the model.
         """
-        "*** YOUR CODE HERE ***"
+        while True:
+            for x, y in dataset.iterate_once(self.batchSize):
+                loss = self.get_loss(x, y)
+                gradient = nn.gradients(loss, self.params)
+                for i in range(len(self.params)):
+                    self.params[i].update(gradient[i], -self.learningRate)
+            if dataset.get_validation_accuracy() >= 0.97:
+                return
 
 class LanguageIDModel(object):
     """
@@ -200,7 +224,19 @@ class LanguageIDModel(object):
         self.languages = ["English", "Spanish", "Finnish", "Dutch", "Polish"]
 
         # Initialize your model parameters here
-        "*** YOUR CODE HERE ***"
+
+        # taken from https://github.com/zhiming-xu/CS188/blob/master/p5-ml/models.py as I still
+        # dont fully understand how to choose these values
+        self.learningRate = 0.1
+        self.batchSize = 100
+        self.w0 = nn.Parameter(self.num_chars, 256)
+        self.b0 = nn.Parameter(1, 256)
+        self.w_x = nn.Parameter(self.num_chars, 256)
+        self.w_h = nn.Parameter(256, 256)
+        self.b = nn.Parameter(1, 256)
+        self.output_w = nn.Parameter(256, len(self.languages))
+        self.output_b = nn.Parameter(1, len(self.languages))
+        self.params = [self.w0, self.b0, self.w_x, self.w_h, self.b, self.output_w, self.output_b]
 
     def run(self, xs):
         """
@@ -231,7 +267,12 @@ class LanguageIDModel(object):
             A node with shape (batch_size x 5) containing predicted scores
                 (also called logits)
         """
-        "*** YOUR CODE HERE ***"
+        h_i = nn.ReLU(nn.AddBias(nn.Linear(xs[0], self.w0), self.b0))
+        for c in xs[1:]:
+            h_i = nn.ReLU(nn.AddBias(nn.Add(nn.Linear(c, self.w_x), nn.Linear(h_i, self.w_h)), self.b))
+        # where did this final term come from?
+        output = nn.AddBias(nn.Linear(h_i, self.output_w), self.output_b)
+        return output
 
     def get_loss(self, xs, y):
         """
@@ -247,10 +288,17 @@ class LanguageIDModel(object):
             y: a node with shape (batch_size x 5)
         Returns: a loss node
         """
-        "*** YOUR CODE HERE ***"
+        return nn.SoftmaxLoss(self.run(xs), y)
 
     def train(self, dataset):
         """
         Trains the model.
         """
-        "*** YOUR CODE HERE ***"
+        while True:
+            for x, y in dataset.iterate_once(self.batchSize):
+                loss = self.get_loss(x, y)
+                gradient = nn.gradients(loss, self.params)
+                for i in range(len(self.params)):
+                    self.params[i].update(gradient[i], -self.learningRate)
+            if dataset.get_validation_accuracy() >= 0.85:
+                return
