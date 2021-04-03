9ea66f060648a8b5773fee593f948c646346ee86
Ian Macaulay <ismacaul@gmail.com>
2021-04-03 14:06:42 -0600

    [rust] Implemented game of life

--- src/rust/wasm-game-of-life/src/lib.rs
+++ src/rust/wasm-game-of-life/src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use std::fmt;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -13,7 +14,130 @@ extern "C" {
     fn alert(s: &str);
 }
 
+// repr allows us to represent each cell as a single byte
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                let next_cell = match (cell, live_neighbors) {
+                    // Rule 1: any live cell with fewer than 2 neighbors dies
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+
+                    // Rule 2: any live cell with 2 or 3 neighbors lives on
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+
+                    // Rule 3: any live cell with more than 3 neighbors dies
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+
+                    // Rule 3: any dead cell with exactly 3 neighbors becomes alive
+                    (Cell::Dead, 3) => Cell::Alive,
+
+                    // otherwise no change
+                    (otherwise, _) => otherwise,
+                };
+
+                next[idx] = next_cell;
+            }
+        }
+
+        self.cells = next;
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
+    fn get_index(&self, row: u32, col: u32) -> usize {
+        return (row * self.width + col) as usize;
+    }
+
+    fn live_neighbor_count(&self, row: u32, col: u32) -> u8 {
+        let mut count = 0;
+
+        // use self.height - 1 to subtract by -1, letting the modulo operator
+        // handle the subtraction to prevent unsigned integer underflow
+        for drow in [self.height - 1, 0, 1].iter().cloned() {
+            for dcol in [self.width - 1, 0, 1].iter().cloned() {
+                if drow == 0 && dcol == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + drow) % self.height;
+                let neighbor_col = (col + dcol) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+
+        count
+    }
+}
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }
 }
