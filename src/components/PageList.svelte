c3e96034450397122e5b28a8a1a86f934483f2ca
Ian Macaulay <ismacaul@gmail.com>
2023-06-23 17:11:49 -0600

    Started organizing home page into a grid

--- src/components/PageList.svelte
+++ src/components/PageList.svelte
@@ -21,18 +21,20 @@
   }
 </style>
 
-{#each pages as page}
-  {#if page.section}
-    {#if subsection}
-      <h3>{page.section}</h3>
-    {:else}
-      <h2>{page.section}</h2>
+<div class="grid grid-cols-4 {subsection ? 'col-span-4' : ''}">
+  {#each pages as page}
+    {#if page.section}
+      {#if subsection}
+        <h3 class="text-sm col-span-4">{page.section}</h3>
+      {:else}
+        <h2 class="text-lg font-thin col-span-4">{page.section}</h2>
+      {/if}
+      {#if page.description}
+        <p class="col-span-4">{@html page.description}</p>
+      {/if}
+      <svelte:self pages={page.pages} subsection={true} />
+    {:else if page.url}
+      <a class="nav-link" href="/{page.url}">{page.title}</a>
     {/if}
-    {#if page.description}
-      <p>{@html page.description}</p>
-    {/if}
-    <svelte:self pages={page.pages} subsection={true}/>
-  {:else if page.url}
-    <a class="nav-link" href="/{page.url}">{page.title}</a>
-  {/if}
-{/each}
+  {/each}
+</div>
--- src/components/Section.svelte
+++ src/components/Section.svelte
@@ -10,14 +10,14 @@
     margin-bottom: 20px;
   }
 
-  h1 {
-    font-weight: 300;
-    margin: 0 0 0.5em 0;
-  }
+  /* h1 { */
+  /*   font-weight: 300; */
+  /*   margin: 0 0 0.5em 0; */
+  /* } */
 </style>
 
 <div class="container">
-  <h1>{title}</h1>
+  <h1 class="font-light text-2xl">{title}</h1>
 
   <slot />
 
